efcd8a3d0d0e533ebb1e9597b88df3752cd86b0a
Nicolas <nicovc20@gmail.com>
2021-10-11 21:49:14 -0500

    Crear lazy inicializacion en el root

--- app/src/main/java/com/example/daggerudemy/common/composition/AppCompositionRoot.kt
+++ app/src/main/java/com/example/daggerudemy/common/composition/AppCompositionRoot.kt
@@ -1,5 +1,6 @@
 package com.example.daggerudemy.common.composition
 
+import androidx.annotation.UiThread
 import com.example.daggerudemy.Constants
 import com.example.daggerudemy.networking.StackoverflowApi
 import com.example.daggerudemy.questions.FetchDetailQuestionUseCase
@@ -11,27 +12,39 @@ import retrofit2.converter.gson.GsonConverterFactory
  * Clase que contiene las propiedades, inicializaciones de casos de usos
  * globales en la aplicación lo cual permite tener un mejor encapsulamiento de esta necesidad
  * y asi obtener mayor legibilidad  y mantenibilidad.
+ *
+ * el decorador de UiThread obliga a que cualquier clase que haga uso de la clase debe ser llamada en el
+ * UiThread
  */
-class AppCompositionRoot {
 
+
+@UiThread
+class AppCompositionRoot {
     /**
-     * Propiedad que realiza un Build a un objeto de Retrofit
+     * Propiedad que realiza un Build a un objeto de Retrofit y valida que solo se cree una sola instancia cuando
+     * se tenga que usar y asi reducir el tiempo de iniciación de la aplicación
+     * Usando la palabra lazy delegate , que se encarga de realizar una inicialización segunra en el Treath
+     * y asi cuando se use por primera vez se creara la instancia y cuando se tenga que usar de nuevo se retorna la instancia que
+     * se creo inicialmente
      */
-    private val retrofit : Retrofit = Retrofit.Builder()
-        .baseUrl(Constants.BASE_URL)
-        .addConverterFactory(GsonConverterFactory.create())
-        .build()
+    private val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
     /**
      * Propiedad que crea un objeto tipo retrofit con la capacidad de realizar consultas a
      * StackOverflow
      */
-    private val stackoverflowApi : StackoverflowApi = retrofit.create(StackoverflowApi::class.java)
+    private val stackoverflowApi: StackoverflowApi by lazy { retrofit.create(StackoverflowApi::class.java) }
 
     /**
      * Se hace uso de la palabra get para que cuando se se llame esta propiedad se cree la instancia y a su vez se destruya
      * lo que va a evitar fugas de memoria y posibles crash
      */
-    val fetchQuestionsUseCase get()  = FetchQuestionsUseCase(stackoverflowApi)
+    val fetchQuestionsUseCase get() = FetchQuestionsUseCase(stackoverflowApi)
 
     val fetchDetailQuestionUseCase get() = FetchDetailQuestionUseCase(stackoverflowApi)
 
